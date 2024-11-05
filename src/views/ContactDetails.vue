8ef8ee9e5d70fca510684a6c9ffcd5300db46d01
Emmanuel Guim III <emmanuelguim.third@gmail.com>
2024-11-05 11:20:36 +0800

    Implemented data rendering components

---
+++ src/views/ContactDetails.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+
+const route = useRoute()
+const contact = ref(null)
+const error = ref(null)
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/contacts/${route.params.id}`,
+    )
+
+    contact.value = response.data
+  } catch (err) {
+    if (err.response && err.response.status === 404) {
+      error.value = 'User ID not found'
+    } else {
+      error.value = 'There was a problem with the fetch operation'
+    }
+  }
+})
+</script>
+
+<template>
+  <button @click="$router.back()">Back</button>
+
+  <div v-if="contact">
+    <h1>{{ contact.name }}</h1>
+    <p>Email: {{ contact.email }}</p>
+    <p>Phone: {{ contact.phoneNumber }}</p>
+  </div>
+  <div v-else-if="error">
+    <p>{{ error }}</p>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -1,5 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+const contacts = ref([])
+const router = useRouter()
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/contacts')
+    contacts.value = response.data
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+  }
+})
+
+const openContactDetails = id => {
+  router.push(`/contacts/${id}`)
+}
+</script>
 
 <template>
-  <h1>Contact List</h1>
+  <div>
+    <h1>Contact List</h1>
+    <ul>
+      <li
+        v-for="contact in contacts"
+        :key="contact.id"
+        @click="openContactDetails(contact.id)"
+      >
+        {{ contact.name }} - {{ contact.email }} - {{ contact.phoneNumber }}
+      </li>
+    </ul>
+  </div>
 </template>
